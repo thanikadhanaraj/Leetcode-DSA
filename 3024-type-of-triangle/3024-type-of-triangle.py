34651ee7e5bdcc400f3c37238782907458ff951c
thanika <thanika.dhanaraj@gmail.com>
2026-09-19 21:37:53 +0530

    Time: 4 ms (1.6%), Space: 19.2 MB (54.64%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.py
+++ 3024-type-of-triangle/3024-type-of-triangle.py
@@ -1,15 +1,14 @@
 class Solution:
-    def triangleType(self, nums: List[int]) -> str:
-        # Step 1: Check if the sides can actually form a valid triangle
-        if (nums[0] + nums[1] <= nums[2] or 
-            nums[0] + nums[2] <= nums[1] or 
-            nums[1] + nums[2] <= nums[0]):
+    def triangleType(self, nums):
+        a, b, c = nums
+
+        if a + b <= c or a + c <= b or b + c <= a:
             return "none"
-        
-        # Step 2: Classify the triangle using control statements
-        if nums[0] == nums[1] == nums[2]:
+
+        if a == b == c:
             return "equilateral"
-        elif nums[0] == nums[1] or nums[1] == nums[2] or nums[0] == nums[2]:
+        elif a == b or b == c or a == c:
             return "isosceles"
         else:
             return "scalene"
+        
